96a6015dad910bf05100ecff764812264a9c2c89
bjr <brendanrooney11@gmail.com>
2019-07-13 01:47:08 -0500

    Updated the update file and added emacs init

--- update.py
+++ update.py
@@ -4,14 +4,14 @@ import sys
 def main():
     print(sys.argv)
     if "emacs" in sys.argv:
-        subprocess.run(["mv", "~/.emacs.d/init.el", "emacs/"])
-        subprocess.run(["mv", "-r", "~/.emacs.d/init_files/", "emacs/"])
+        subprocess.run(["cp", '/home/bjr/.emacs.d/init.el', "emacs/"])
+        subprocess.run(["cp", "-r", "/home/bjr/.emacs.d/init_files/", "emacs/"])
     if "zsh" in sys.argv:
-        subprocess.run(["mv", "~/.zshrc", "zsh/"])
+        subprocess.run(["cp", "/home/bjr/.zshrc", "zsh/"])
     if "i3" in sys.argv:
-        subprocess.run(["mv", "-r", "~/.config/i3", "i3/"])
+        subprocess.run(["cp", "-r", "/home/bjr/.config/i3", "i3/"])
     if "i3-blocks" in sys.argv:
-        subprocess.run(["mv", "-r", "~/.config/i3blocks", "i3blocks/"])
+        subprocess.run(["cp", "-r", "/home/bjr/.config/i3blocks", "i3blocks/"])
 
     
 if __name__ == "__main__":
